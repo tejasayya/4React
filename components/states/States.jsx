b84690e5632bd41e3f8553a2397f12b782758fa6
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-02-14 11:41:37 -0500

    Working states filter

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './States.css';
 
-/**
- * Define States, a React component of Project 4, Problem 2. The model
- * data for this view (the state names) is available at
- * window.models.states.
- */
-class States extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('window.models.states', window.models.states);
-    const states = window.models.states(); 
-    console.log(states);
-  }
+function States() {
+  console.log('window.models.states', window.models.states);
+  const states = window.models.states(); 
 
-  render() {
-    return (
-      <div>
-        <label htmlFor="filter">Input:</label>
-        <input type="text" id="filter" name="filter"/>
-        <ul>
-          {window.models.states().map((state, index) => (
-            <li key={index}>{state}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  let filteredStates = states.filter(state => state.toLowerCase().includes(filter));
+  filteredStates.sort((a, b) => a.localeCompare(b));
+
+  const statesList = filteredStates.map((state, index) => (
+    <li key={index}>{state}</li>
+  ));
+
+  return (
+    <div>
+      <label htmlFor="filter">Input:</label>
+      <input type="text" id="filter" name="filter" value={filter} onChange={handleFilterChange}/>
+      <ul>
+        {statesList}
+      </ul>
+    </div>
+  );
 }
 
 export default States;
