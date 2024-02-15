0061103090f405349d864294dbec8357cabc4bcb
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-02-15 12:49:46 -0500

    Added message when no states found

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -11,21 +11,25 @@ function States() {
     setFilter(e.target.value);
   };
 
-  let filteredStates = states.filter(state => state.toLowerCase().includes(filter));
+  let filteredStates = states.filter(
+    state => state.toLowerCase().includes(filter));
   filteredStates.sort((a, b) => a.localeCompare(b));
 
-  const statesList = filteredStates.map((state, index) => (
+  const statesListItems = filteredStates.map((state, index) => (
     <li key={index}>{state}</li>
   ));
+
+  let statesList = <ul>{statesListItems}</ul>;
+  if(filteredStates.length === 0) {
+    statesList = <p>No matches found</p>;
+  }
 
   return (
     <div>
       <label htmlFor="filter">Input:</label>
       <input type="text" id="filter" name="filter" value={filter} onChange={handleFilterChange}/>
       <p>Substring: {filter}</p>
-      <ul>
-        {statesList}
-      </ul>
+      {statesList}
     </div>
   );
 }
