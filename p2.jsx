501c22587ea0ec9da7f67a1aba76be018b980e0f
Sayya Teja Swaroop <63931564+tejasayya@users.noreply.github.com>
2024-02-16 03:09:44 -0500

    Update p2.jsx

--- p2.jsx
+++ p2.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from '../components/header/header';
+
 import States from './components/states/States';
 
 ReactDOM.render(
